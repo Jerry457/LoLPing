bd19d1ca3437a0bec54780fbcf1cbf41fc8c959b
Jerry <2234834467@qq.com>
2022-10-03 16:33:00 +0800

    upload files

---
+++ main/assets.lua
@@ -0,0 +1,15 @@
+PrefabFiles = {
+    "ping_music"
+}
+
+Assets = {
+    Asset("ATLAS", "images/lolpingwheel.xml"),
+    Asset("IMAGE", "images/lolpingwheel.tex"),
+    Asset("ATLAS", "images/lolpingtab.xml"),
+    Asset("IMAGE", "images/lolpingtab.tex"),
+    Asset("ANIM", "anim/question.zip"),
+    Asset("ANIM", "anim/danger.zip"),
+    Asset("ANIM", "anim/help.zip"),
+    Asset("ANIM", "anim/ontheway.zip"),
+    Asset("ANIM", "anim/dogtag.zip"),
+}
